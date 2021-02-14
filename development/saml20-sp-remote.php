6ef2ace73bcaf45a8e4eb13f3fe3f28798e85f48
Lucas Alber <lucasd.alber@gmail.com>
2021-02-14 13:09:25 +0100

    Fix SAML for Artemis

--- development/saml20-sp-remote.php
+++ development/saml20-sp-remote.php
@@ -13,7 +13,7 @@ $metadata['http://gitlab'] = array(
 );
 $metadata['artemis'] = array(
     /*Set registration id to testidp*/
-    'AssertionConsumerService' => 'http://localhost/login/saml2/sso/testidp',
+    'AssertionConsumerService' => 'http://localhost:8080/login/saml2/sso/testidp',
     'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
     'simplesaml.nameidattribute' => 'uid',
     'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:persistent',
